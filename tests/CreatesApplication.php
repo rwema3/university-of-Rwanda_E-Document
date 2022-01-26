22a54bc85cdf6d244553197035310d21af7fbfc6
rwema3 <rwemadominik3@gmail.com>
2022-01-26 14:38:06 +0200

    DB changes
    DB changes

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Console\Kernel;
 trait CreatesApplication
 {
     /**
-     * Creates the application.
+     * Creates the 
      *
      * @return \Illuminate\Foundation\Application
      */
